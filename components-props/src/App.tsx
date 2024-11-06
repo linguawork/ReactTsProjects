7552dcd87a2b24447ce06a9d44a5fd80c4355fb3
linguawork <mixedworldcitizen@gmail.com>
2024-11-06 21:33:23 +0100

    Component with a single prop added

--- components-props/src/App.tsx
+++ components-props/src/App.tsx
@@ -2,11 +2,40 @@ import React from 'react';
 
 // import './App.css';
 
+
+//variant 3
+interface Name {
+  personName:string
+}
+
+
+//variant2
+// type Name = {
+//   personName:string
+// }
+
+function Card({personName}:Name){
+  return <h1>{personName}</h1>
+  
+}
+
+//variant 1
+// function Card({personName}:{personName:string}){
+//   return <h1>{personName}</h1>
+  
+// }
+
+
 function App() {
   return (
-    <h1>
-      Hello world!
-    </h1>
+    <div>
+
+        <h1>
+          Test!
+        </h1>
+
+    <Card personName="Darwin"/>
+    </div>
 
   );
 }
