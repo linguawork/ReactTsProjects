720bce26639d694e7b10925bc35c3aaf67215b75
Arnette Reggie <areggie@c2r11s10.42wolfsburg.de>
2024-11-12 15:49:18 +0100

    useState with TS, event with TS

--- usestatets/src/App.tsx
+++ usestatets/src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+//typescript type is imported to work with eventypes
+import { ChangeEvent } from 'react';
+
+//interface is written with Capital letter
+interface UserInput{
+  user: string, 
+  pass: string
+}
+
 function App() {
+
+  const [userInput, setUserInput] = useState<UserInput>({user:"username", pass:"123214"})
+
+  //event типа ChangeEvent c подтипом Input
+  function setUserName(e: ChangeEvent<HTMLInputElement>){
+    //the function get the current state of the input
+    // by spreading the current state and replaces with the new user input
+    setUserInput({...userInput, user: e.target.value})
+  } 
+
+  // this is for testing
+  // console.log(userInput)
+
+
+  function setUserPass(e: ChangeEvent<HTMLInputElement>){
+    setUserInput({...userInput, pass: e.target.value})
+  }
+
+
   return (
+    //create User password form
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>User</h3>
+      {/* если навести на е в функции то можно увидеть тип,
+       это как подсказка для правильной типизации  event */}
+      <input type="text" onChange={(e) => setUserName(e)} />
+      <h3>Password</h3>
+      <input type="text" onChange={(e) => setUserPass(e) }/>
     </div>
   );
 }
