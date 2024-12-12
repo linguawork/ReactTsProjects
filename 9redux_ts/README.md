9e69c3a46540668436020b2a4816898b38c132c6
linguawork <mixedworldcitizen@gmail.com>
2024-12-12 11:53:04 +0100

    The project with redux and typescript is created: typizatioin of userReducer: TYPE->INTERFACE->ENUM

---
+++ 9redux_ts/README.md
@@ -0,0 +1,29 @@
+# Getting Started with Create React App
+
+```
+npx create-react-app project_name --template typescript
+```
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### Delete unnecessary files: 
+./reportWebVitals
+
+### install:
+```
+npm i --save-dev @types/react 
+npm install --save-dev @types/react-dom@latest
+npm i @types/react-redux redux react-redux redux-thunk axios
+```
+
+This can be enough to work with the project folder: redux+ts+react
---
+++ 9redux_ts/src/store/reducers/userReducer.ts
@@ -0,0 +1,87 @@
+/**
+ Плюс работы с тайпскриптом, можно отловить ошибку на этапе разработки
+ */
+
+//константы нужны для более удобной обработки ошибок
+        // const FETCH_USERS = "FETCH_USERS"
+        // const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS"
+        // const FETCH_USERS_ERROR = "FETCH_USERS_ERROR"
+// we can remove them as we use enum
+
+
+
+//interface for State and for returning in function
+interface userState{
+    users:any[],
+    loading:boolean,
+    error: null | string
+}
+
+
+enum UserActionTypes{
+    FETCH_USERS = "FETCH_USERS", 
+    FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS", 
+    FETCH_USERS_ERROR = "FETCH_USERS_ERROR"
+
+}
+
+//as action.payload can have anything, it is better type it
+interface FetchUsersAction{
+    // type: FETCH_USERS //we can use typeof here, but better off with ENUM
+    type: UserActionTypes.FETCH_USERS
+}
+
+interface FetchUsersSuccessAction{
+    type: UserActionTypes.FETCH_USERS_SUCCESS, 
+    payload: any[] // array of anytype
+}
+
+interface FetchUsersErrorAction{
+    type: UserActionTypes.FETCH_USERS_ERROR, 
+    error: string
+}
+
+//interface for action is not specific for every type of action
+//so we dont need it, we go for every enum value
+// interface userAction {
+//     type: string
+//     payload?: any
+// }
+
+//type is limited to 3 interfaces
+//TYPE has 3 INTERFACES that have ENUM
+type UserAction =  FetchUsersAction | FetchUsersSuccessAction | FetchUsersErrorAction
+
+
+const initialState:userState ={
+    users: [], 
+    loading: false, //flag to see if it is loading
+    error: null //error
+}
+
+export const userReducer = (state = initialState, action: UserAction): userState =>{
+ 
+ //commented as we dont use constants, we will change to ENUM
+    // switch(action.type){
+    //     case FETCH_USERS:
+    //         return {loading: true, error:null, users:[]}
+    //     case FETCH_USERS_SUCCESS:
+    //         return {loading: false, error:null, users:action.payload}
+    //     case FETCH_USERS_ERROR:
+    //         return {loading: true, error:action.payload, users:[]}
+    //     default:
+    //         return state
+    // }
+
+    switch(action.type){
+        case UserActionTypes.FETCH_USERS:
+            return {loading: true, error:null, users:[]}
+        case UserActionTypes.FETCH_USERS_SUCCESS:
+            return {loading: false, error:null, users:action.payload}
+        case UserActionTypes.FETCH_USERS_ERROR:
+            return {loading: true, error:action.payload, users:[]}
+        default:
+            return state
+    }
+
+}
