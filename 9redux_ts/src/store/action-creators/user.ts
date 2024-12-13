e87d56782afa0f79a3f483a77a747afd3f9391ce
linguawork <mixedworldcitizen@gmail.com>
2024-12-13 12:15:27 +0100

    writing user.ts, fetching users with axios

---
+++ 9redux_ts/src/store/action-creators/user.ts
@@ -0,0 +1,38 @@
+/**
+    Применяем redux-thunk
+
+    Он для асинхронного кода
+
+    суть его в том, что можем принимать dispatch, 
+    не импортируя его
+*/
+
+
+//this is the Dispatch for typization 
+import { Dispatch } from "redux"
+import { UserAction, UserActionTypes } from "../../types/userReducerTypes"
+import axios from "axios"
+
+
+export const fetchUsers = () =>{
+    //Dispatch<UserAction>, UserAction is type, which has 3 interfaces, inside which there are enums
+    return async(dispatch:Dispatch<UserAction>) => {
+    
+    try{
+        //passing enum values 
+        dispatch({type:UserActionTypes.FETCH_USERS })
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+    }catch(e){
+        //when you catch an error, there should be payload:
+        dispatch({type:UserActionTypes.FETCH_USERS, payload: "Error happened when loading users" })
+    }
+
+}
+
+export const fetchCustomers =()=>{
+    return function (dispatch){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(json => dispatch(addManyCustomersActionCreator(json)))
+    }
+}
